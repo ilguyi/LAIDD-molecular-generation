bf4614ac41853c76485cc914555b1302b98dcc48
Il Gu Yi <ilgu.yi.219@gmail.com>
2021-08-23 13:42:13 +0900

    added trainer and train module to train.py

--- laiddmg/train.py
+++ laiddmg/train.py
@@ -8,15 +8,18 @@ from datetime import datetime
 from laiddmg import (
   get_train_args,
   set_output_dir,
-  CharRNNConfig,
-  VAEConfig,
   Tokenizer,
+  CharRNNConfig,
   CharRNNModel,
+  VAEConfig,
   VAEModel,
+  get_rawdataset,
+  get_dataset,
+  TRAINER_MAPPING,
   measure_duration_time,
 )
 
-from . import logging
+from . import logging_utils
 
 
 def main():
@@ -26,21 +29,21 @@ def main():
   args = get_train_args()
   model_type = sys.argv[1]
   args = set_output_dir(model_type, args)
-  logger = logging.get_logger(__name__, os.path.join(args.output_dir, 'out.log'))
+  logger = logging_utils.get_logger(__name__, os.path.join(args.output_dir, 'out.log'))
 
   logger.info(args)
   logger.info(f'model type: {model_type}')
   logger.info(f'use device: {args.device}')
 
   assert model_type in ['char_rnn', 'vae']
+  tokenizer = Tokenizer()
   if model_type == 'char_rnn':
     config = CharRNNConfig()
-    tokenizer = Tokenizer()
     model = CharRNNModel(config)
   else:
     config = VAEConfig()
-    tokenizer = Tokenizer()
     model = VAEModel(config)
+
   print(config)
   print(tokenizer('c1cccc1c'))
   print(model)
@@ -49,11 +52,19 @@ def main():
   print(model.dtype)
   print(model.num_parameters())
 
-  inputs = tokenizer(['c1cccc1c', 'c1ccc1c'])
-  outputs, hiddens = model(**inputs)
-  print(outputs.shape)
-  print(hiddens[0].shape)
-  print(hiddens[1].shape)
+  # get raw dataset (SMILES)
+  train = get_rawdataset('train')
+
+  # get PyTorch Dataset
+  train_dataset = get_dataset(train, tokenizer)
+
+  # get trainer
+  trainer = TRAINER_MAPPING[model_type]
+  t = trainer(model=model,
+              args=args,
+              train_dataset=train_dataset,
+              tokenizer=tokenizer)
+  t.train()
 
   end_time = datetime.now()
   measure_duration_time(end_time - start_time)
